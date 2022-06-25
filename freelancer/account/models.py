24c530e7e76d3303b4ff5f4970df0b3df2ab2675
reza-tanha <adamak.tnh@gmail.com>
2022-06-25 21:44:10 +0430

    add app account and write user model

---
+++ freelancer/account/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    email = models.EmailField(
+        max_length=100,
+        unique=True,
+        verbose_name="ایمیل")
+
+    privilege = models.IntegerField(
+        default=0,
+        verbose_name='امتیاز کاربر')
+
+    user_pro = models.BooleanField(
+        default=False,
+        verbose_name='کاربر ویژه')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['last_name', 'username']
--- freelancer/config/settings.py
+++ freelancer/config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'job.apps.JobConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
