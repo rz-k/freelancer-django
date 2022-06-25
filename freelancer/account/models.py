3093b3d54330ce1f6e40d295e6647b7a66f547a6
reza-tanha <adamak.tnh@gmail.com>
2022-06-25 22:14:57 +0430

    change all path base and index

--- freelancer/account/models.py
+++ freelancer/account/models.py
@@ -2,20 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+# class User(AbstractUser):
 
-    email = models.EmailField(
-        max_length=100,
-        unique=True,
-        verbose_name="ایمیل")
+#     email = models.EmailField(
+#         max_length=100,
+#         unique=True,
+#         verbose_name="ایمیل")
 
-    privilege = models.IntegerField(
-        default=0,
-        verbose_name='امتیاز کاربر')
+#     privilege = models.IntegerField(
+#         default=0,
+#         verbose_name='امتیاز کاربر')
 
-    user_pro = models.BooleanField(
-        default=False,
-        verbose_name='کاربر ویژه')
+#     user_pro = models.BooleanField(
+#         default=False,
+#         verbose_name='کاربر ویژه')
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['last_name', 'username']
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['last_name', 'username']
