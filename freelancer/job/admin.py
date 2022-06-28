9cd107538d22539a739c951515d5bfa67dadf280
zankoatzanko <zankoan4@gmail.com>
2022-06-29 02:53:50 +0430

    :hammer: Refactor the code and add a new method to get job titles.

--- freelancer/job/admin.py
+++ freelancer/job/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
-# Register your models here.
+from .models import Apply, Category, Job
+
+
+class CategoryFilter(SimpleListFilter):
+    """
+       Filter by parent or children Category.
+    """
+    title = 'Category'
+    parameter_name = 'category_type'
+
+    def lookups(self, request, model_admin):
+        return (('parent', 'Parent'),
+                ("children", "Children"))
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+        
+        if self.value().lower() == 'children':
+            verified_users = Category.objects.exclude(parent=None)
+            return verified_users
+            
+        elif self.value().lower() == 'parent':
+            unverified_users = Category.objects.filter(parent=None)
+            return unverified_users
+
+
+@admin.register(Category)
+class JobCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "slug", "name", "status", "created")
+    list_editable = ("status", )
+    list_display_links = ("slug",)
+    list_filter = ("status", CategoryFilter)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "price")
+    list_editable = ("price", "status")
+    list_display_links = ("title",)
+    list_filter = ("status",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Apply)
+class ApplyJobAdmin(admin.ModelAdmin):
+    list_display = ("id", "get_job_title", "status", "finish_time")
+    list_editable = ("status", )
+    list_filter = ("status", )
+    list_display_links = ("get_job_title",)
--- freelancer/job/models.py
+++ freelancer/job/models.py
@@ -1,12 +1,12 @@
-from django.db import models
-from account.models import User
-from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 from django.contrib.postgres.fields import ArrayField
+from django.db import models
+from django.utils.text import slugify
 
 
 def image_job_directory_path(instance, filename: str) -> "File Path":
     """
-    File location to store user Job.
+    The file location where the logo or photo of the job should be stored. 
     
     Returns:
         file-path: str
@@ -14,55 +14,126 @@ def image_job_directory_path(instance, filename: str) -> "File Path":
     path = 'users/{0}/job/{1}'
     return path.format(instance.user.id, filename)
 
-class Category(models.Model):
-    parrent = models.ForeignKey('self', on_delete=models.CASCADE, default=None, null=True, blank=True,verbose_name='زیردسته', related_name='childernt')
-    title = models.CharField(max_length=200, verbose_name='عنوان دسته')
-    slug = models.CharField(max_length=200, unique=True, verbose_name='آدرس دسته')
-    status = models.BooleanField(default=True, verbose_name='فعال شود ؟')
-    position = models.IntegerField(verbose_name='پوزیشن',unique=True)
 
-    class Meta:
-        verbose_name = 'دسته بندی'
-        verbose_name_plural = 'دسته بندی ها'
-        ordering = ['position']
+class Category(models.Model):
+    parent = models.ForeignKey(
+        to='self',
+        on_delete=models.CASCADE,
+        default=None,
+        null=True,
+        blank=True,
+        verbose_name='زیردسته',
+        related_name='children')
+    
+    name = models.CharField(
+        max_length=200,
+        verbose_name='عنوان دسته')
+    
+    slug = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='آدرس دسته')
+    
+    status = models.BooleanField(
+        default=True,
+        verbose_name='فعال شود ؟')
+    
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Job(models.Model):
+    user = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="user_job")
+    
+    title = models.CharField(
+        max_length=100,
+        verbose_name="عنوان پروژه")
+    
+    slug = models.SlugField(
+        max_length=120,
+        blank=True,
+        null=True,
+        default=None,
+        allow_unicode=True)
+
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        related_name='job_category')
+
+    tag = ArrayField(
+        models.CharField(
+            max_length=20,
+            blank=True,
+            null=True
+        ),
+        blank=True,
+        null=True,
+        size=5,
+        verbose_name="تگ های پروژه")
+
+    description = models.TextField(
+        max_length=250,
+        verbose_name='توضیحات پروژه')
+
+    image = models.ImageField(
+        upload_to=image_job_directory_path,
+        blank=True,
+        null=True,
+        verbose_name="عکس پروژه یا لوگو شرکت",)
+
+    status = models.BooleanField(
+        default=False,
+        verbose_name="وضعیت پروژه")
+
+    price = models.BigIntegerField(verbose_name="بودجه")
+    created = models.DateTimeField(auto_now=True)
+
 
     def __str__(self) -> str:
         return self.title
 
 
-
-
-
-class Job(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_job")
-    title = models.CharField(max_length=100, verbose_name="تایتل")
-    slug = models.SlugField(max_length=120,default="no-slug",blank=True, null=True, allow_unicode=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='job_category')
-    tag = ArrayField( models.CharField(max_length=20,blank=True,null=True),
-            blank=True,
-            null=True,
-            size=3,
-            verbose_name="تگ های پروژه")
-    description = models.TextField(verbose_name='توضیحات پروژه', max_length=250)
-    price = models.BigIntegerField(verbose_name="بودجه")
-    image = models.ImageField(verbose_name="عکس پروژه", upload_to=image_job_directory_path, blank=True, null=True)
-    created = models.DateTimeField(auto_now=True)
-    status = models.BooleanField(default=False, verbose_name="انجام شده ؟")
-
-
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.job_title, allow_unicode=True)
-        super(Job, self).save(*args, **kwargs)
+        """
+            Create a new job with a 'Persian' dynamic slug.
+        """
+        self.slug = slugify(value=self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
 
 
-class ApplayJob(models.Model):
-    HHOISES = (
-        ('accept', 'پذیرفته شد ✅'),
-        ('reject', 'رد شد ❌'),
-        ('wait', 'در حال انتظار ⌛️')
+class Apply(models.Model):
+    APPLY_STATUS = (
+        ('accept', 'پذیرفته شد'),
+        ('reject', 'رد شد'),
+        ('wait', 'در حال انتظار')
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_applay")
-    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name="job_applay")
-    created = models.DateTimeField(auto_now=True)
+    
+    user = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="user_applay")
+    
+    job = models.ForeignKey(
+        to=Job,
+        on_delete=models.CASCADE,
+        related_name="job_applay")
+    
+    status = models.CharField(
+        max_length=20,
+        choices=APPLY_STATUS,
+        verbose_name='وضعیت درخواست',
+        default='wait')
+    
     finish_time = models.IntegerField(verbose_name="زمان انجام پروژه")
-    status = models.CharField(choices=HHOISES, verbose_name='وضعیت درخواست', default='wait')
+    created = models.DateTimeField(auto_now=True)
+
+
+    def get_job_title(self):
+        return self.job.slug
