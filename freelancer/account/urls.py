761ef9db4494694a59a875503e3c12ba3f8e1525
zankoatzanko <zankoan4@gmail.com>
2022-06-29 02:44:57 +0430

    Add job urls.

--- freelancer/account/urls.py
+++ freelancer/account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import *
-
+from .views import (add_job, dashboard, edit_profile, login_user, logout_user,
+                    manage_candidate, manage_job, register_user, user_messages)
 
 app_name = 'account'
 
@@ -16,5 +16,4 @@ urlpatterns = [
     path('dashboard/manage-candidate/', manage_candidate, name='manage-candidate'),
     path('dashboard/user-messages/', user_messages, name='user-messages'),
     path('dashboard/edit-profile/', edit_profile, name='edit-profile'),
-
 ]
