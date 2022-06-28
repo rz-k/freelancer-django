1067cf5ddded6f9766c221c0009d0b520ea93599
zankoatzanko <zankoan4@gmail.com>
2022-06-28 16:39:59 +0430

    :sparkles: Add ApprovedUserFilter to filter users based on approval status

--- freelancer/account/admin.py
+++ freelancer/account/admin.py
@@ -1,29 +1,48 @@
-import imp
 from django.contrib import admin
-from django.contrib.admin import StackedInline
-from .models import User, Profile
+from django.contrib.admin import SimpleListFilter, StackedInline
 
+from .models import Profile, User
 
 
 class UserProfileInline(StackedInline):
     model = Profile
 
+
+class ApprovedUserFilter(SimpleListFilter):
+    """
+       Filter approved user.
+    """
+    title = 'Approved User'
+    parameter_name = 'user_status'
+
+    def lookups(self, request, model_admin):
+        return (('approved', 'Approved'),
+                ("unverified", "Unverified"))
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+        
+        if self.value().lower() == 'approved':
+            verified_users = User.objects.filter(user_profile__approved=True)
+            return verified_users
+            
+        elif self.value().lower() == 'unverified':
+            unverified_users = User.objects.filter(user_profile__approved=False)
+            return unverified_users
+
+
 @admin.register(User)
 class UserProfileAdmin(admin.ModelAdmin):
-    inlines = [UserProfileInline, ]
-    list_display = ["user_id","email","first_name", "last_name", "score", "user_approved"]
-    list_editable = ("first_name","last_name","score",)
+    inlines = (UserProfileInline, )    
+    list_display = ("id", "email", "first_name", "last_name", "score", "is_verified")
+    list_editable = ("first_name", "last_name","score")
+    list_filter = (ApprovedUserFilter,)
+    list_display_links = ("email",)
 
-    def user_approved(self, record):
-        prf = Profile.objects.filter(user=record).first()
-        return prf.approved
-
-    def user_id(self, record):
-        return record.id
-
-    # @admin.action(description="make approved true")
-    # def MakeApprovedTrue(self, request, queryset):
-    #     print(queryset)
-    #     # prf = Profile.objects.filter(user=record).first()
-    #     rows = queryset.update(available=True)
-    #     # self.message_user(request, f"{rows} Product is Avaleble Success !")
+    
+    def is_verified(self, record):
+        user_profile = Profile.objects.filter(user=record).first()
+        return user_profile.approved
+    
+    is_verified.boolean = True
