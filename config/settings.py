c3fc7c9af8b0950f91439063d1cdc737df7032e3
zankoatzanko <zankoan4@gmail.com>
2022-07-01 04:59:45 +0430

    :heavy_plus_sign: Add context_processors to handle breadcrumbs.

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'config.context_processors.get_breadcrumb',
             ],
         },
     },
@@ -103,7 +104,6 @@ QUILL_CONFIGS = {
         'modules': {
             'syntax': True,
             'toolbar': [
-                ['link'],
                 [
                     {'align': []},
                     {'list': 'ordered'}, {'list': 'bullet'},
