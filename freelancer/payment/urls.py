7f6f786f4e4a7b5c335851fa0936452e91c05f94
zankoatzanko <zankoan4@gmail.com>
2022-07-05 04:12:34 +0430

    :hammer: Refactor the payment urls and views.

--- freelancer/payment/urls.py
+++ freelancer/payment/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import  ZarinpalRequest,ZarinpalVerifi
+from .views import  ZarinpalSendPayRequest, ZarinpalVerify
 
 
 app_name ='payment'
+
 urlpatterns = [
-    path('send/<int:id>', ZarinpalRequest.as_view(), name="send_request" ),    
-    path('verify/', ZarinpalVerifi.as_view(), name="verify" ),
+    path('send/<int:id>', ZarinpalSendPayRequest.as_view(), name="send_request"),
+    path('verify/', ZarinpalVerify.as_view(), name="verify"),
 ]
--- freelancer/payment/views.py
+++ freelancer/payment/views.py
@@ -1,116 +1,164 @@
-import requests
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.shortcuts import HttpResponse, get_object_or_404, redirect, render
-from django.views import View
-from freelancer.job.models import Job
-from freelancer.payment.models import PaymentAccount, PaymentJob
 import json
 
+import requests
+from django.conf import settings
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
+
+from .models import PaymentAccount, PaymentJob
+
+
 class ZarinpalInfo:
-    MERCHANT = settings.MERCHANT
-    ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
-    ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
-    ZP_API_STARTPAY = "https://www.zarinpal.com/pg/StartPay/{authority}"
-    # ZP_CALLBACK = f'https://gray-city.ir/pay/verify/'
-    ZP_CALLBACK = f'https://ce96-154-6-16-197.ngrok.io/pay/verify/'
-    HEADERS = {
-            "accept": "application/json",
-            "content-type": "application/json'"
-            }
+    amount = "1000"
+    mobile = "phone"
+    description = "description"
+
+    zarinpal_merchant = settings.MERCHANT
+    zarinpal_api_request = "https://api.zarinpal.com/pg/v4/payment/request.json"
+    zarinpal_api_verify = "https://api.zarinpal.com/pg/v4/payment/verify.json"
+    zarinpal_api_startpay = "https://www.zarinpal.com/pg/StartPay/{authority}"
+    zarinpal_callback = 'https://ce96-154-6-16-197.ngrok.io/pay/verify/'
+    zarinpal_headers = {
+        "accept": "application/json",
+        "content-type": "application/json'"}
+
+    zarinpal_send_data = {
+        "merchant_id": zarinpal_merchant,
+        "description": description,
+        "callback_url": zarinpal_callback,
+        "amount": "",
+        "metadata": {"mobile": mobile, "email": ""}}
 
 
-class ZarinpalRequest(View, ZarinpalInfo):
-
+class ZarinpalSendPayRequest(View, ZarinpalInfo):
     def get(self, request, id):
-        job = get_object_or_404(Job, user=request.user, id=id)
-        self.amount = "1000"
-        self.description = "description"
-        self.email = request.user.email
-        self.mobile = "phone"
-        req_data = {
-            "merchant_id": settings.MERCHANT,
-            "amount": self.amount,
-            "callback_url": ZarinpalInfo.ZP_CALLBACK,
-            "description": self.description,
-            "metadata": {"mobile": self.mobile, "email": self.email}
-        }
-        req = requests.post(url=ZarinpalInfo.ZP_API_REQUEST, data=json.dumps(
-            req_data), headers=ZarinpalInfo.HEADERS)
+        job = get_object_or_404(klass=Job, user=request.user, id=id)
         
-        authority = req.json()['data']['authority']
-        if len(req.json()['errors']) == 0:
-            pay = PaymentJob(user=request.user,job=job, price=self.amount, authority=authority)
-            pay.save()
-            return redirect(ZarinpalInfo.ZP_API_STARTPAY.format(authority=authority))
+        #=> Pack the data.
+        data = self.zarinpal_send_data
+        data["amount"] = self.amount
+        data["metadata"]["email"] = request.user.email
+
+        #=> send information
+        response = self.send_information(data=data)
+
+        #=> validate response
+        if response['errors']:
+            return JsonResponse({
+                "error_status_code": response['errors']['code'],
+                "error_message": response['errors']['message']})
         else:
-            e_code = req.json()['errors']['code']
-            e_message = req.json()['errors']['message']
-            return HttpResponse(f"ارور کد: {e_code}, ارور مسیج: {e_message}")
+            #=> Create a successful payment for the job.
+            authority = response['data']['authority']
+            success_url = self.zarinpal_api_startpay.format(authority=authority)
+            payment = PaymentJob(
+                user=request.user,
+                job=job,
+                price=self.amount,
+                authority=authority).save()
 
-    def post(self, request, id):
-        return redirect('job:home')
+            return redirect(success_url)
 
 
-class ZarinpalVerifi(View, ZarinpalInfo):
+    def send_information(self, data):
+        """Send ``zarinpal`` information and return the response.
+        """
+        data = json.dumps(data)
+        response = requests.post(
+            url=self.zarinpal_api_request,
+            data=data,
+            headers=self.zarinpal_headers
+        ).json()
 
-    def get(self , request):       
-        t_status = request.GET.get('Status')
-        t_authority = request.GET['Authority']
-        pay = PaymentJob.objects.get(authority=t_authority)
-        
-        if request.GET.get('Status') == 'OK':
-            req_data = {
-                "merchant_id": ZarinpalInfo.MERCHANT,
-                "amount": pay.price,
-                "authority": t_authority
+        return response
+
+
+    def post(self, request, id, success_url="job:home"):
+        return redirect(success_url)
+
+
+
+class ZarinpalVerify(View, ZarinpalInfo):
+    error_template_name = "payment/error.html"
+    success_template_name = "payment/success.html"
+
+    def get(self , request):
+        status = request.GET.get("Status").lower()
+        authority = request.GET["Authority"]
+        payment = PaymentJob.objects.get(authority=authority)
+
+        if status == 'ok':
+            data = {
+                "merchant_id": self.zarinpal_merchant,
+                "amount": payment.price,
+                "authority": authority
             }
+            response = requests.post(
+                url=self.zarinpal_api_verify,
+                data=json.dumps(data),
+                headers=self.zarinpal_headers
+            ).json()
+            self.handel_verify_response(response=response, payment=payment, authority=authority)
 
-            req = requests.post(url=ZarinpalInfo.ZP_API_VERIFY, data=json.dumps(req_data), headers=ZarinpalInfo.HEADERS)
-            if len(req.json()['errors']) == 0:
-                t_status = req.json()['data']['code']
-                if t_status == 100:
-                    pay.payed =True
-                    pay.job.payed=True
-                    pay.job.save()
-                    pay.save()
-                    
-                    res = {
-                        'authority':t_authority,
-                        'price' : pay.price,
-                        'email':pay.user.email
-                    }
-
-                    return render(request, 'payment/success.html', {'result':res})
-
-                elif t_status == 101:
-                    res = {
-                        'message': str(req.json()['data']['message']) ,
-                        "authority": t_authority
-                    }
-
-                    return render(request, 'payment/error.html', {'result':res})
-                else:
-                    res = {
-                        'message': str(req.json()['data']['message']),
-                        "authority": t_authority
-                     }
-                    return render(request, 'payment/error.html', {'result':res})
-            else:
-                e_code = req.json()['errors']['code']
-                e_message = req.json()['errors']['message']
-                res = {
-                    'message': str(e_message),
-                    "authority": t_authority
-                    }
-                return render(request, 'payment/error.html', {'result':res})
         else:
-            res = {
-                    'message': "تراکنش با خطا مواجه شده است",
-                    "authority": t_authority
-                    }
-            return render(request, 'payment/error.html', {'result':res})
+            context = {
+                "message": "تراکنش با خطا مواجه شده است",
+                "authority": authority
+            }
+            return render(request=request,
+                template_name=self.error_template_name, context={"result": context})
 
 
-# def sh(request):
-#     return render(request , 'zarinpal/success.html')
+    def handel_verify_response(self, response: "json", payment: [PaymentAccount, PaymentJob], authority: str):
+        """Handel the success verification responses and errors.
+        
+        Response Status Code:
+            100:
+                Code 100 that means the transaction is successful.
+
+            101 and *:
+                Code 101 that means the transaction was successful and
+                was verified once before, and this is the second time.
+        """
+        if response['errors']:
+            error_code = response['errors']['code']
+            error_message = response['errors']['message']
+            context = {
+                'message': error_message,
+                'authority': authority
+            }
+            return render(request=request,
+                template_name=self.error_template_name, context={"result": context})
+
+        else:
+            status_code = response['data']['code']
+            if status_code == 100:
+                payment.payed = True
+                payment.job.payed = True
+                payment.job.save()
+                payment.save()
+
+                context = {
+                    'authority': authority,
+                    'price' : payment.price,
+                    'email': payment.user.email
+                }
+                return render(request=request,
+                    template_name=self.success_template_name, context={"result": context})
+
+            elif status_code == 101:
+                context = {
+                    'message': response['data']['message'],
+                    'authority': authority
+                }
+                return render(request=request,
+                    template_name=self.error_template_name, context={"result": context})
+
+            else:
+                context = {
+                    'message': response['data']['message'],
+                    'authority': authority
+                }
+                return render(request=request,
+                    template_name=self.error_template_name, context={"result": context})
