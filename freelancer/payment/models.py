db1684736c0f6ce03d6022c93bed84937a41844b
zankoatzanko <zankoan4@gmail.com>
2022-07-08 23:23:43 +0430

    :hammer: Update payament model and views

--- freelancer/payment/models.py
+++ freelancer/payment/models.py
@@ -1,16 +1,32 @@
-from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth import get_user_model
 from freelancer.job.models import Job
 from freelancer.project.models import Project
 
 
 class PaymentAccount(models.Model):
     User = get_user_model()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='پرداخت کننده', related_name='user_account_pay')
+
+    user = models.ForeignKey(
+        to=User,
+        related_name='user_account_pay',
+        on_delete=models.CASCADE,
+        verbose_name='پرداخت کننده')
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ پرداخت')
+
+    authority = models.CharField(
+        max_length=120,
+        verbose_name='توکن خرید')
+
+    payed = models.BooleanField(
+        default=False,
+        verbose_name='پرداخت شده ؟')
+
     price = models.IntegerField(verbose_name='مبلغ(ریال)')
-    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ پرداخت')
-    authority = models.CharField(max_length=120,verbose_name='توکن خرید')
-    payed = models.BooleanField(default=False, verbose_name='پرداخت شده ؟')
+
 
     class Meta:
         verbose_name = 'User Payment'
@@ -22,14 +38,34 @@ class PaymentAccount(models.Model):
 
 class PaymentJob(models.Model):
     User = get_user_model()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='پرداخت کننده', related_name='user_job_pay')
-    job = models.ForeignKey(Job, on_delete=models.CASCADE, verbose_name='جاب', related_name='job_pay')
+
+    user = models.ForeignKey(
+        to=User,
+        related_name='user_job_pay',
+        on_delete=models.CASCADE,
+        verbose_name='پرداخت کننده')
+
+    job = models.ForeignKey(
+        to=Job,
+        related_name='job_pay',
+        on_delete=models.CASCADE,
+        verbose_name='جاب')
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ پرداخت')
+
+    authority = models.CharField(
+        max_length=120,
+        verbose_name='توکن خرید')
+
+    paid = models.BooleanField(
+        default=False,
+        verbose_name='پرداخت شده ؟')
+
     price = models.IntegerField(verbose_name='مبلغ(ریال)')
-    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ پرداخت')
-    authority = models.CharField(max_length=120,verbose_name='توکن خرید')
-    payed = models.BooleanField(default=False, verbose_name='پرداخت شده ؟')
-    
-    
+
+
     class Meta:
         verbose_name = 'Job Payment'
         verbose_name_plural = "Job Payments"
@@ -40,14 +76,34 @@ class PaymentJob(models.Model):
 
 class PaymentProject(models.Model):
     User = get_user_model()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='پرداخت کننده', related_name='user_project_pay')
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='پروژه', related_name='project_pay')
+
+    user = models.ForeignKey(
+        to=User,
+        related_name='user_project_pay',
+        on_delete=models.CASCADE,
+        verbose_name='پرداخت کننده')
+
+    project = models.ForeignKey(
+        to=Project,
+        related_name='project_pay',
+        on_delete=models.CASCADE,
+        verbose_name='پروژه')
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ پرداخت')
+
+    authority = models.CharField(
+        max_length=120,
+        verbose_name='توکن خرید')
+
+    paid = models.BooleanField(
+        default=False,
+        verbose_name='پرداخت شده ؟')
+
     price = models.IntegerField(verbose_name='مبلغ(ریال)')
-    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ پرداخت')
-    authority = models.CharField(max_length=120,verbose_name='توکن خرید')
-    payed = models.BooleanField(default=False, verbose_name='پرداخت شده ؟')
-    
-    
+
+
     class Meta:
         verbose_name = 'Project Payment'
         verbose_name_plural = "Projects Payments"
--- freelancer/payment/views.py
+++ freelancer/payment/views.py
@@ -1,30 +1,31 @@
 import json
-from tkinter.messagebox import NO
+
 import requests
 from django.conf import settings
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import PaymentAccount, PaymentJob, PaymentProject
-from freelancer.job.models import Job
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import JsonResponse
+from django.views import View
 from freelancer.project.models import Project
-from django.conf import settings
+from freelancer.job.models import Job
+
+from .models import PaymentAccount, PaymentJob, PaymentProject
 
 
 class ZarinpalInfo:
     amount = "1000"
+    project_amount = "1000"
     mobile = "phone"
-    description = "description"
 
+    description = "description"
     zarinpal_merchant = settings.MERCHANT
     zarinpal_api_request = "https://api.zarinpal.com/pg/v4/payment/request.json"
     zarinpal_api_verify = "https://api.zarinpal.com/pg/v4/payment/verify.json"
     zarinpal_api_startpay = "https://www.zarinpal.com/pg/StartPay/{authority}"
-    zarinpal_callback = settings.BASE_SITE_URL+'/pay/verify/'
+    zarinpal_callback = settings.BASE_SITE_URL + "/pay/verify/"    
     zarinpal_headers = {
         "accept": "application/json",
-        "content-type": "application/json'"}
+        "content-type": "application/json"}
 
     zarinpal_send_data = {
         "merchant_id": zarinpal_merchant,
@@ -36,26 +37,18 @@ class ZarinpalInfo:
 
 class ZarinpalSendPayRequest(LoginRequiredMixin,View, ZarinpalInfo):
     def get(self, request, uuid):
-        # job , project = None
-        print(uuid)
-        project = None
-        job = Job.objects.filter(user=request.user,uuid=uuid).first()
+        job = Job.objects.filter(user=request.user, uuid=uuid).first()
         if not job:
-            project = Project.objects.filter(user=request.user,uuid=uuid).first()
-            
-        
-
-        print(job)
-        print(project)
-        print("="*100)
+            project = get_object_or_404(klass=Project, user=request.user, uuid=uuid)
+            self.amount += self.project_amount
 
         data = self.zarinpal_send_data
         data["amount"] = self.amount
         data["metadata"]["email"] = request.user.email
-        print(data)
+
         #=> send information
         response = self.send_information(data=data)
-        print(response)
+
         #=> validate response
         if response['errors']:
             return JsonResponse({
@@ -65,25 +58,24 @@ class ZarinpalSendPayRequest(LoginRequiredMixin,View, ZarinpalInfo):
             #=> Create a successful payment for the job.
             authority = response['data']['authority']
             success_url = self.zarinpal_api_startpay.format(authority=authority)
-            print(success_url)
-            if project:
-                payment = PaymentProject(
-                    user=request.user,
-                    project=project,
+
+            if job:
+                payment = PaymentJob(user=request.user,
+                    job=job, 
                     price=self.amount,
                     authority=authority).save()
             else:
-                payment = PaymentJob(
-                    user=request.user,
-                    job=job,
+                payment = PaymentProject(user=request.user,
+                    project=project,
                     price=self.amount,
                     authority=authority).save()
 
-
             return redirect(success_url)
 
+
     def send_information(self, data):
-        """Send ``zarinpal`` information and return the response.
+        """
+        Send ``zarinpal`` information and return the response.
         """
         data = json.dumps(data)
         response = requests.post(
@@ -103,46 +95,39 @@ class ZarinpalSendPayRequest(LoginRequiredMixin,View, ZarinpalInfo):
 class ZarinpalVerify(View, ZarinpalInfo):
     error_template_name = "payment/error.html"
     success_template_name = "payment/success.html"
-    
 
     def get(self , request):
-        status = request.GET.get("Status").lower()
-        authority = request.GET["Authority"]
-
-        payjob, payproj = None
-
-        payjob = PaymentJob.objects.filter(authority=authority).first()
-        if not payjob:
-            payproj = PaymentProject.objects.filter(authority=authority).first()
-
+        status = str(request.GET.get("Status")).lower()
+        authority = request.GET.get("Authority")
+        
+        paying_job = PaymentJob.objects.filter(authority=authority).first()
+        if not paying_job:
+            paying_project = PaymentProject.objects.filter(authority=authority).first()
+        
         if status == 'ok':
-            if payjob:
-                data = {
-                    "merchant_id": self.zarinpal_merchant,
-                    "amount": payjob.price,
-                    "authority": authority
-                }
-            else:
-                data = {
-                    "merchant_id": self.zarinpal_merchant,
-                    "amount": payproj.price,
-                    "authority": authority
-                }
-
-
-
+            data = {
+                "merchant_id": self.zarinpal_merchant,
+                "amount": paying_job.price if paying_job else paying_project.price,
+                "authority": authority
+            }
 
             response = requests.post(
                 url=self.zarinpal_api_verify,
                 data=json.dumps(data),
                 headers=self.zarinpal_headers
             ).json()
+            
+            payment = paying_job
+            rel = "job"
+            if not paying_job:
+                rel = "project"
+                payment = paying_project
 
-
-            if payjob:
-                self.handel_verify_response(request, response=response, payment=payjob, authority=authority)
-            else:
-                self.handel_verify_response(request,response=response, payment=payproj, authority=authority)
+            data = self.handel_verify_response(
+                response=response, authority=authority,
+                rel=rel, payment=payment)
+            return render(request=request, template_name=data["template_name"], context=data["result"])
+            
         else:
             context = {
                 "message": "تراکنش با خطا مواجه شده است",
@@ -152,8 +137,9 @@ class ZarinpalVerify(View, ZarinpalInfo):
                 template_name=self.error_template_name, context={"result": context})
 
 
-    def handel_verify_response(self,request,  response: "json", payment, authority: str):
-        """Handel the success verification responses and errors.
+    def handel_verify_response(self, response: "json", authority: str, rel: str, payment: [PaymentAccount, PaymentJob, PaymentProject]):
+        """
+        Handel the success verification responses and errors.
         
         Response Status Code:
             100:
@@ -170,17 +156,18 @@ class ZarinpalVerify(View, ZarinpalInfo):
                 'message': error_message,
                 'authority': authority
             }
-            return render(request=request,
-                template_name=self.error_template_name, context={"result": context})
+            return {"template_name": self.error_template_name, "result": context}
 
         else:
-
-            status_code = response['data']['code']
+            status_code = response['data']['code']            
             if status_code == 100:
-
-                payment.payed = True
-                payment.job.payed = True
-                payment.job.save()
+                if rel == "job":
+                    payment.job.paid = True
+                    payment.job.save()
+                else:
+                    payment.project.paid = True
+                    payment.project.save()
+                payment.paid = True
                 payment.save()
 
                 context = {
@@ -188,21 +175,18 @@ class ZarinpalVerify(View, ZarinpalInfo):
                     'price' : payment.price,
                     'email': payment.user.email
                 }
-                return render(request=request,
-                    template_name=self.success_template_name, context={"result": context})
+                return {"template_name": self.success_template_name, "result": context}                
 
             elif status_code == 101:
                 context = {
                     'message': response['data']['message'],
                     'authority': authority
                 }
-                return render(request=request,
-                    template_name=self.error_template_name, context={"result": context})
+                return {"template_name": self.error_template_name, "result": context}                
 
             else:
                 context = {
                     'message': response['data']['message'],
                     'authority': authority
                 }
-                return render(request=request,
-                    template_name=self.error_template_name, context={"result": context})
+                return {"template_name": self.error_template_name, "result": context}                
