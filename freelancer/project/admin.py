6827fc6b4d3b8879b5607f711ce5c0da2f8da13e
zankoatzanko <zankoan4@gmail.com>
2022-07-09 01:22:54 +0430

    :zap: Update project Admin

--- freelancer/project/admin.py
+++ freelancer/project/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+
 from .models import Project
 
 
-admin.site.register(Project)
+@admin.register(Project)
+class JobCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "urgent", "highlight", "private", "paid")
+    list_editable = ("status", "urgent", "highlight", "private", "paid")
+    list_display_links = ("title",)
+    list_filter = ("status", "paid")
+    prepopulated_fields = {"slug": ("title",)}
