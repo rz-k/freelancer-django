55fea510ae087db997869613e7ecdc1b97f27760
reza-tanha <adamak.tnh@gmail.com>
2022-07-11 23:37:40 +0430

    make model faq

--- freelancer/faq/models.py
+++ freelancer/faq/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django_quill.fields import QuillField
 
-# Create your models here.
+
+class Faq(models.Model):
+
+    question = models.CharField(
+        max_length=100,   
+        verbose_name='سوال متداول'      
+    )
+    answer = QuillField(
+        max_length=20000,
+        verbose_name='جواب سوال متداول')
+
+    position = models.IntegerField(
+        unique=True,
+        verbose_name='موقعیت سوال'
+    )
+
+    def __str__(self) -> str:
+        return self.question
+
