67426d1f433548b8cd60ba17f448a8469e888052
zankoatzanko <zankoan4@gmail.com>
2022-07-14 21:29:40 +0430

    :sparkles: :heavy_plus_sign: Change the order of project form fields and add a few widgets.

--- freelancer/project/forms.py
+++ freelancer/project/forms.py
@@ -1,44 +1,49 @@
 from django import forms
 from django.contrib.postgres.forms import SimpleArrayField
 from django_quill.forms import QuillFormField
-from .models import Project, Category
+from freelancer.project.models import Category
+from .models import Project
 
 
 class AddProjectForm(forms.Form):
-
     title = forms.CharField(
-        label="عنوان پروژه (کار)",
+        label_suffix="عنوان پروژه (کار)",
         max_length=100,
-        widget=forms.TextInput(            
-            attrs={"class":"input-text", "placeholder":"عنوان پروژه یا شغل"}))
-    
-    
-    category = forms.ModelChoiceField(
-        label="دسته بندی",
-        queryset=Category.objects.filter(status=True),
-        widget=forms.Select(attrs={"class":"input-text"}))
+        widget=forms.TextInput(
+            attrs={"class": "input-text", "placeholder": "عنوان پروژه یا شغل"}))
 
-    tags = SimpleArrayField(
-        forms.CharField(max_length=100),
-        label_suffix="تگ های پروژه(حداکثر 5 مورد)")
-    
-    description = QuillFormField(
-        max_length=10000,
-        label="توضیحات",
-        error_messages={"required":"لطفا توضیحات پروژه را قرار دهید"})
+    category = forms.ModelChoiceField(
+        label_suffix="دسته بندی",
+        queryset=Category.objects.filter(status=True),
+        widget=forms.Select(attrs={"class": "input-text"}))
 
     budget = forms.CharField(
         max_length=100,
         error_messages={"invalid": "بودجه  در نظر گرفته شده را وارد نمایید."},
-        label="بودجه در نظر گرفته شده (ریال|توافقی)",
+        label_suffix="بودجه در نظر گرفته شده (ریال|توافقی)",
         widget=forms.TextInput(
-                attrs={"class":"input-text", "placeholder":"1000000"}))
+            attrs={"class": "input-text", "placeholder": "1000000"}))
 
+    tags = SimpleArrayField(
+        label_suffix="تگ های پروژه(حداکثر 5 مورد)",
+        base_field=forms.CharField(max_length=100))
 
-    urgent = forms.BooleanField()
-    highlight = forms.BooleanField()
-    private = forms.BooleanField()
+    private = forms.BooleanField(
+        label_suffix="این پورژه فقط برای افراد دارای اکانت نمایش داده شود؟",
+        widget=forms.CheckboxInput(attrs={"class": "radio-check"}))
 
+    highlight = forms.BooleanField(
+        label_suffix="این پروژه دارای قالب رنگی متفاوتی باشد؟",
+        widget=forms.CheckboxInput(attrs={"class": "radio-check"}))
+
+    urgent = forms.BooleanField(
+        label_suffix="این پروژه دارای تگ فوری باشد؟",
+        widget=forms.CheckboxInput(attrs={"class": "radio-check"}))
+
+    description = QuillFormField(
+        max_length=10000,
+        label_suffix="توضیحات",
+        error_messages={"required": "لطفا توضیحات پروژه را قرار دهید"})
 
     def clean_budget(self):
         """
@@ -54,7 +59,8 @@ class AddProjectForm(forms.Form):
                 int_price = int(budget)
                 return budget
             except ValueError:
-                raise forms.ValidationError("لطفا هزینه پروژه را به ریال وارد نمایید یا از کلید واژه توافقی استفاده کنید")
+                raise forms.ValidationError(
+                    "لطفا هزینه پروژه را به ریال وارد نمایید یا از کلید واژه توافقی استفاده کنید")
 
 
 class EditProjectForm(forms.ModelForm, AddProjectForm):
@@ -65,21 +71,21 @@ class EditProjectForm(forms.ModelForm, AddProjectForm):
 
 class ApplyProjectForm(forms.Form):
     bid_amount = forms.IntegerField(
-        label="مقدار پیشنهادی",
+        label_suffix="مقدار پیشنهادی",
         widget=forms.TextInput(
-            attrs={"class":"input-text", "placeholder": "برای مثال 220,500,000"}),
-        error_messages={"bid_amount":"لطفا مقدار پیشنهادی پروژه را بنویسید"})
+            attrs={"class": "input-text", "placeholder": "برای مثال 220,500,000"}),
+        error_messages={"bid_amount": "لطفا مقدار پیشنهادی پروژه را بنویسید"})
 
     bid_date = forms.IntegerField(
         min_value=1,
-        label="زمان تحویل",
+        label_suffix="زمان تحویل",
         widget=forms.TextInput(
-            attrs={"class":"input-text"}),
-        error_messages={"bid_date":"لطفا زمان تحویل پروژه را بنویسید"})
+            attrs={"class": "input-text"}),
+        error_messages={"bid_date": "لطفا زمان تحویل پروژه را بنویسید"})
 
     description = forms.CharField(
         max_length=500,
-        label="توضیحات",
+        label_suffix="توضیحات",
         widget=forms.Textarea(
-                attrs={"class": "input-text", "placeholder": "توضیحات..."}),
+            attrs={"class": "input-text", "placeholder": "توضیحات..."}),
         error_messages={"required": "لطفا توضیحات خود را برای کارفرما بنویسید"})
