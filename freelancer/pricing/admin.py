0bfcad085c25e2bad024f8cbf0f8d7a632f56c93
reza-tanha <adamak.tnh@gmail.com>
2022-07-19 17:53:36 +0430

    make admin pannel pricing app

--- freelancer/pricing/admin.py
+++ freelancer/pricing/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import PricingPanel, ActivePricingPanel, PricingTag
 
-# Register your models here.
+
+@admin.register(PricingTag)
+class PricingTagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    
+@admin.register(ActivePricingPanel)
+class ActivePricingPanelAdmin(admin.ModelAdmin):
+    list_display = ['user', 'active_panel', 'count', 'expire_time']
+    
+@admin.register(PricingPanel)
+class PricingPanelAdmin(admin.ModelAdmin):
+    list_display = ['panel_type', 'price', 'count', 'days', 'position']
--- freelancer/pricing/models.py
+++ freelancer/pricing/models.py
@@ -1,3 +1,4 @@
+from turtle import position
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
