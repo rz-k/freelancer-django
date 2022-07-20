494416ef277cf309eb72df546a3d6f75effdaf84
zankoatzanko <zankoan4@gmail.com>
2022-07-20 15:26:39 +0430

    Clean up.

--- freelancer/pricing/admin.py
+++ freelancer/pricing/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from .models import PricingPanel, ActivePricingPanel, PricingTag
+
+from .models import ActivePricingPanel, PricingPanel, PricingTag
 
 
 @admin.register(PricingTag)
 class PricingTagAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price']
+    list_display = ["name", "price"]
     
 @admin.register(ActivePricingPanel)
 class ActivePricingPanelAdmin(admin.ModelAdmin):
-    list_display = ['user', 'active_panel', 'count', 'expire_time']
+    list_display = ["user", "active_panel", "count", "expire_time"]
     
 @admin.register(PricingPanel)
 class PricingPanelAdmin(admin.ModelAdmin):
-    list_display = ['panel_type', 'price', 'count', 'days', 'position']
+    list_display = ["panel_type", "price", "count", "days", "position"]
