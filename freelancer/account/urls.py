ab59501ae96aaa96c198e4b25e04b62c16536deb
zankoatzanko <zankoan4@gmail.com>
2022-07-22 12:08:50 +0430

    Change manage-job url to the manage-project

--- freelancer/account/urls.py
+++ freelancer/account/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
     path('dashboard/messages/', user_messages, name='messages'),
     path('dashboard/edit-profile/', edit_profile, name='edit-profile'),
-    path('dashboard/manage-job/', manage_job, name='manage-job'),
+    path('dashboard/manage-project/', manage_project, name='manage-project'),
     path('dashboard/manage-candidate/', manage_candidate, name='manage-candidate'),
     path('dashboard/manage-applays/', manage_applay_send, name='manage-applays'),
 
