a32413f621a6ee5c9cb54f2d632a9e85d16957c9
zankoatzanko <zankoan4@gmail.com>
2022-10-02 19:57:27 +0330

    Renaming

--- freelancer/account/admin.py
+++ freelancer/account/admin.py
@@ -12,12 +12,14 @@ class ApprovedUserFilter(SimpleListFilter):
     """
        Filter approved user.
     """
-    title = 'Approved User'
-    parameter_name = 'user_status'
+    title = "Approved User"
+    parameter_name = "user_status"
 
     def lookups(self, request, model_admin):
-        return (('approved', 'Approved'),
-                ("unverified", "Unverified"))
+        return (
+            ("approved", "Approved"),
+            ("unapproved", "UnApproved")
+        )
 
     def queryset(self, request, queryset):
         if not self.value():
@@ -34,15 +36,15 @@ class ApprovedUserFilter(SimpleListFilter):
 
 @admin.register(User)
 class UserProfileAdmin(admin.ModelAdmin):
-    inlines = (UserProfileInline, )    
-    list_display = ("id", "email", "first_name", "last_name", "score", "is_verified")
-    list_editable = ("first_name", "last_name","score")
+    inlines = (UserProfileInline, )
+    list_display = ("id", "email", "first_name",
+                    "last_name", "score", "is_approved")
+    list_editable = ("first_name", "last_name", "score")
     list_filter = (ApprovedUserFilter,)
     list_display_links = ("email",)
 
-    
-    def is_verified(self, record):
+    def is_approved(self, record):
         user_profile = Profile.objects.filter(user=record).first()
         return user_profile.approved
-    
-    is_verified.boolean = True
+
+    is_approved.boolean = True
