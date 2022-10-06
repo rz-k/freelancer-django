da93ae9291386da14f456af98c242d5ccf37f120
zankoatzanko <zankoan4@gmail.com>
2022-10-06 22:04:53 +0330

    Use GenericForeignKey insted of multi models...

--- freelancer/payment/models.py
+++ freelancer/payment/models.py
@@ -1,73 +1,55 @@
-from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth import get_user_model
-from freelancer.project.models import Project
+from django.db import models
 
 
-class PaymentAccount(models.Model):
+class Payment(models.Model):
     User = get_user_model()
 
     user = models.ForeignKey(
         to=User,
-        related_name='user_account_pay',
+        related_name="account_payer",
         on_delete=models.CASCADE,
-        verbose_name='پرداخت کننده')
+        verbose_name="پرداخت کننده")
 
-    created = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='تاریخ پرداخت')
+    payment_type = models.ForeignKey(
+        to=ContentType,
+        related_name="payment_type",
+        on_delete=models.CASCADE,
+        verbose_name="پرداختی برای")
+
+    payment_object = GenericForeignKey(
+        ct_field="payment_type",
+        fk_field="object_id")
 
     authority = models.CharField(
         max_length=120,
-        verbose_name='توکن خرید')
+        verbose_name="توکن خرید")
 
-    payed = models.BooleanField(
-        default=False,
-        verbose_name='پرداخت شده ؟')
-
-    price = models.IntegerField(verbose_name='مبلغ(ریال)')
-
-
-    class Meta:
-        verbose_name = 'User Payment'
-        verbose_name_plural = 'User Payments'
-
-    def __str__(self) -> str:
-        return str(self.user)
-
-
-class PaymentProject(models.Model):
-    User = get_user_model()
-
-    user = models.ForeignKey(
-        to=User,
-        related_name='user_project_pay',
-        on_delete=models.CASCADE,
-        verbose_name='پرداخت کننده')
-
-    project = models.ForeignKey(
-        to=Project,
-        related_name='project_pay',
-        on_delete=models.CASCADE,
-        verbose_name='پروژه')
-
-    created = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='تاریخ پرداخت')
-
-    authority = models.CharField(
-        max_length=120,
-        verbose_name='توکن خرید')
+    card_pan = models.CharField(
+        max_length=30,
+        verbose_name="شماره کارت")
 
     paid = models.BooleanField(
         default=False,
-        verbose_name='پرداخت شده ؟')
+        verbose_name="وضعیت پرداخت")
 
-    price = models.IntegerField(verbose_name='مبلغ(ریال)')
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="تاریخ پرداخت")
 
+    object_id = models.PositiveIntegerField(
+        null=True,
+        verbose_name="ایدی پرداختی مرتبط")
+
+    price = models.IntegerField(verbose_name="مبلغ(ریال)")
+    ref_id = models.IntegerField(verbose_name="شماره تراکنش")
+    fee = models.IntegerField(verbose_name="کارمزد")
 
     class Meta:
-        verbose_name = 'Project Payment'
-        verbose_name_plural = "Projects Payments"
+        verbose_name = "Payments"
+        verbose_name_plural = "Payments"
 
     def __str__(self) -> str:
-        return str(self.project)
+        return f"{self.payment_type} | {self.user.username}"
