da61d897788c282ab8f94bff1ed88f47d9ac0b0b
zankoatzanko <zankoan4@gmail.com>
2022-10-08 14:34:52 +0330

    Rename the apply count and apply counter

--- freelancer/pricing/admin.py
+++ freelancer/pricing/admin.py
@@ -6,11 +6,13 @@ from .models import ActivePricingPanel, PricingPanel, PricingTag
 @admin.register(PricingTag)
 class PricingTagAdmin(admin.ModelAdmin):
     list_display = ["name", "price"]
-    
+
+
 @admin.register(ActivePricingPanel)
 class ActivePricingPanelAdmin(admin.ModelAdmin):
-    list_display = ["user", "active_panel", "count", "expire_time"]
-    
+    list_display = ["user", "active_panel", "apply_counter", "expire_time"]
+
+
 @admin.register(PricingPanel)
 class PricingPanelAdmin(admin.ModelAdmin):
-    list_display = ["panel_type", "price", "count", "days", "position"]
+    list_display = ["panel_type", "price", "apply_count", "days", "position"]
